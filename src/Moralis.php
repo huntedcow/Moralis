76ad0a49791d69d9c86b874c346a8fba42c2814c
Andrew Mulholland <andrew@huntedcow.com>
2021-11-10 01:47:37 +0000

    Structure changes (remove static)

--- src/Moralis.php
+++ src/Moralis.php
@@ -5,34 +5,40 @@ namespace HuntedCow\Moralis;
 class Moralis
 {
     /** @var string  Server URL from on https://admin.moralis.io/servers */
-    private static $SERVER_URL = 'https://deep-index.moralis.io/api/v2/';
+    private $serverUrl;
     /** @var string  API Key obtained from https://admin.moralis.io/web3Api */
-    private static $API_KEY = '';
+    private $apiKey;
+
+    public function __construct(string $apiKey, string $serverUrl = 'https://deep-index.moralis.io/api/v2/')
+    {
+        $this->apiKey       = $apiKey;
+        $this->serverUrl    = $serverUrl;
+    }
 
     /**
      * Sets the server url
      * @param string $serverUrl
      */
-    public static function setServer(string $serverUrl) : void
+    public function setServer(string $serverUrl) : void
     {
-        self::$SERVER_URL = $serverUrl;
+        $this->serverUrl = $serverUrl;
     }
 
     /**
      * Returns the current server url
      * @return string Server Url
      */
-    public static function getServer() : string
+    public function getServer() : string
     {
-        return self::$SERVER_URL;
+        return $this->serverUrl;
     }
 
     /**
      * Sets the API key
      * @param string $apiKey
      */
-    public static function setApiKey(string $apiKey) : void
+    public function setApiKey(string $apiKey) : void
     {
-        self::$API_KEY = $apiKey;
+        $this->apiKey = $apiKey;
     }
 }
